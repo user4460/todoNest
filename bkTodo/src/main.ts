7262d18c74a5abb7a980da75f384ccaf37083332
yoshiaki.n <52188314+user4460@users.noreply.github.com>
2022-10-12 13:03:05 +0800

    nest crudの途中まで

--- bkTodo/src/main.ts
+++ bkTodo/src/main.ts
@@ -1,15 +1,26 @@
 //NestFactoryとは、Nest.jsのアプリケーションを作成するためのファクトリークラスです。NestFactoryを使用して、アプリケーションを作成し、アプリケーションを起動することができます。
 import { NestFactory } from '@nestjs/core';
-//AppModuleとは、アプリケーションのルートモジュールです。アプリケーションのルートモジュールは、アプリケーションの全ての機能を提供するモジュールです。
+
+//AppModuleとは、アプリケーションのルートモジュールです。アプリケーションの全ての機能を提供するモジュールです。
 import { AppModule } from './app.module';
+
+//FastifyAdapterとは、fastifyを使用してNest.jsアプリケーションを作成するためのアダプタークラスです。
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 //bootstrapとは、アプリケーションを起動するためのメソッドです。
 //asyncとは、非同期処理を行うためのメソッドです。
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
 
-  //追記　console.logとは、コンソールに文字列を出力するメソッドです。
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  /* FastifyAdapterを入れたapp
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  */
+  await app.listen(3000);
 }
 bootstrap();
