ea0375164f975cdc89cee11e1b2f7a805a42cf6c
yoshiaki.n <52188314+user4460@users.noreply.github.com>
2022-10-12 14:14:07 +0800

    コメント

--- bkTodo/src/task/task.module.ts
+++ bkTodo/src/task/task.module.ts
@@ -3,6 +3,7 @@ import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
 
 //providersにPrismaServiceを追加
+//https://zenn.dev/devgeeeen/articles/125a076f81b0df
 import { PrismaService } from './../prisma.service';
 
 @Module({
