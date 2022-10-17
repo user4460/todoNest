ddbe39986954699687fc9d35494750a919a3642d
yoshiaki.n <52188314+user4460@users.noreply.github.com>
2022-10-17 11:39:27 +0800

    todo test

--- frtodo/cypress/e2e/index/todo.cy.js
+++ frtodo/cypress/e2e/index/todo.cy.js
@@ -27,6 +27,45 @@ describe("Authentication test in index page", () => {
    });
 });
 
+/*
+describe("todoを作成する", () => {
+   const email = "test@example.com";
+   const password = "123456";
+
+   it("Login by firebase authentication", () => {
+      console.log(email);
+   });
+});
+
+describe("todoを編集する", () => {
+   const email = "test@example.com";
+   const password = "123456";
+
+   it("Login by firebase authentication", () => {
+      console.log(email);
+   });
+});
+
+describe("todoを削除する", () => {
+   const email = "test@example.com";
+   const password = "123456";
+
+   it("Login by firebase authentication", () => {
+      console.log(email);
+   });
+});
+
+describe("todoをチェックする", () => {
+   const email = "test@example.com";
+   const password = "123456";
+
+   it("Login by firebase authentication", () => {
+      console.log(email);
+   });
+});
+
+*/
+
 describe("Authentication test in index page", () => {
    const email = "test@example.com";
    const password = "123456";
